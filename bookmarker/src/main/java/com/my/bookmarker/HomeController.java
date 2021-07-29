c278184f7a1b606b8992b64cc5a3e27a78047b5d
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2021-07-30 01:08:23 +0900

    [추가] Rest API용 스프링 프레임 구성
    + REST API용 스프링 프레임 구성
    
    !! 주의 !!
    pom.properties 내부에 m2e.projectLocation에 적혀있는 E\:\\bookMarker\\back\\를 반드시 각자 프로젝트 파일 경로로 바꿔줄 것(중요)
    
    !! 주의 !!
    배포용 서버는 알아서 파서 할 것 (본인은 톰캣 9.0 씀)
    포트도 알아서 할 것

---
+++ bookmarker/src/main/java/com/my/bookmarker/HomeController.java
@@ -0,0 +1,59 @@
+package com.my.bookmarker;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Handles requests for the application home page.
+ */
+@RestController
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	@RequestMapping(value="/test/02", method= RequestMethod.GET)
+	public List<String> testGet01() {
+		List<String> result = new ArrayList<String>();
+		result.add("테스트");
+		result.add("입니다");
+		return result;
+	}
+	
+	@RequestMapping(value="/test/03/{variable}", method= RequestMethod.GET)
+	public List<String> testGet02(@PathVariable String variable) {
+		List<String> result = new ArrayList<String>();
+		result.add("테스트");
+		result.add(variable);
+		result.add("입니다");
+		return result;
+	}
+	
+}
